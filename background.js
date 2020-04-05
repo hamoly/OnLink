7decb8d8c10c0af9864aac0afeb524c8434c6020
Hamoly <Hamoly@hotmail.com>
2020-04-05 21:35:35 +0200

    styling, status

--- background.js
+++ background.js
@@ -1,18 +1,18 @@
-var db, tabid, tolink;
+var db, tabid, msgPort;
 ////////
 // INDEXEDDB
 // Create indexedDB Request if not found
-let dbOpenRequest = indexedDB.open("onlinks", 2);
+let dbOpenRequest = indexedDB.open('onlinks', 2);
 // Create objectstore if not found
 dbOpenRequest.onupgradeneeded = e => { 
   // saving opened DB to "db" variable
-  let db = event.target.result;
+  db = e.target.result;
   // Creating objectStore on this database
-  let objectStore = db.createObjectStore("shortenlink", { keyPath: 'key' });
+  db.createObjectStore('shortenlink', { keyPath: 'key' });
 };
 // Indexeddb Created Successfully
 // saving created DB to "db" variable
-dbOpenRequest.onsuccess = e => db = dbOpenRequest.result;
+dbOpenRequest.onsuccess = e => db = e.target.result;
 // Indexeddb creating request error
 dbOpenRequest.onerror = e => console.log(e.target.error);
 
@@ -20,29 +20,60 @@ dbOpenRequest.onerror = e => console.log(e.target.error);
 // NEW LINK
 // Lestining to messages from popup script
 chrome.runtime.onConnect.addListener(port => {
-  // Checking the recived message port if the same as assigned at popup
-  if(port.name == "newLink") {
-    // Recieving message from popup if port name is the same
-    port.onMessage.addListener(msg =>       
-      // Firing addNewLinkObj function with the recieved message to add the link to indexeddb
-      addNewLinkObj(msg));
-    }
+  // saving port result to use it later
+  msgPort = port;
+  // getting current tab URL
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    activeTabURL = tabs[0].url
+    msgPort.postMessage({taburl : activeTabURL});
   });
 
-// Adding new link object
+  // Recieving message from popup if port name is the same
+  port.onMessage.addListener( msg => {
+    // Checking the recived message port if the same as assigned at popup
+    if(port.name == 'newLink' && msg.type == 'add') {   
+    // Firing addNewLinkObj function with the recieved message to add the link to indexeddb
+      addNewLinkObj(msg);
+    }
+    if(port.name == 'newLink' && msg.type == 'verify') {   
+      // Firing addNewLinkObj function with the recieved message to add the link to indexeddb
+      projectNameVerify(msg)
+    }
+  })
+});
+
+// Add new link object
 const addNewLinkObj = msg => {
-  // Forming new link object and saving it to " newLink " variable 
-  let newLink = ({ key: `http://onlink/${msg.projectName}/${msg.createdAt}`, link: msg.originalLink});
+  // Forming new link object and saving it to "newLink" variable 
+  let newLink = ({ key: `http://o/${msg.projectName}`, link: msg.originalLink });
   // Openning a read/write db transaction, ready for adding the data
-  let transactionRW = db.transaction(["shortenlink"], "readwrite").objectStore("shortenlink");
+  let transactionRW = db.transaction(['shortenlink'], 'readwrite').objectStore('shortenlink');
   // Make a request to add new link object to the object store
   let addNewLinkReq = transactionRW.add(newLink);
   // Link Added Successfully
-  addNewLinkReq.onsuccess = function(e) {console.log(e.target.result)};
+  addNewLinkReq.onsuccess = () => msgPort.postMessage({taburl : activeTabURL, state: 'linkAddedSuccessfully'});
   // Error adding link
-  addNewLinkReq.onerror = function(error) {console.log(error)};
+  addNewLinkReq.onerror = () => msgPort.postMessage({taburl : activeTabURL, state: 'dbError'});
 }
 
+// check if entered project name already exists in DB
+const projectNameVerify = msg => {
+  let projectName = (`http://o/${msg.projectName}`);
+  console.log(projectName);
+  // Openning a read only db transaction, ready for fetching data
+  let transactionRO = db.transaction(['shortenlink'], 'readonly').objectStore('shortenlink');
+  // Fetching original link request
+  let linkValueRequest = transactionRO.get(projectName);
+  // Fetching original request link successed
+  linkValueRequest.onsuccess = () => {
+    if (linkValueRequest.result && linkValueRequest.result.link) {
+      msgPort.postMessage({taburl: activeTabURL, state: 'projectNameExist'});
+    } else {
+      msgPort.postMessage({taburl: activeTabURL, state: 'projectNameDoesnotExist'});
+    }
+  }
+  linkValueRequest.onerror = () => msgPort.postMessage({taburl: activeTabURL, state: 'dbError'});
+};
 ////////
 // REDIRECTING REQUESTED LINK
 // Fetching tab url and id before navigation and fire getLinkfromDB if it contains onlink/*
@@ -52,10 +83,10 @@ chrome.webNavigation.onBeforeNavigate.addListener(tab => {
   // saving tab.url
   tolink = tab.url
   // fire getLinkfromDB if tab url contains onlink/*
-  if(/onlink\/(.)/gi.test(tolink)) {
+  if(/o\/(.)/gi.test(tolink)) {
     getLinkfromDB(tolink);
   } else {
-      console.log("nay");
+      console.log('nay');
     }
 });
 // fetching link and redirect if found
@@ -72,4 +103,24 @@ const getLinkfromDB = tolink => {
     chrome.tabs.update(tabid, {url: linkValue});
   };
   linkValueRequest.onerror = err => console.log(err);
-}
+};
+
+/*
+chrome.omnibox.onInputChanged.addListener(
+  function(text, suggest) {
+    console.log('inputChanged: ' + text);
+    suggest([
+      {content: text + " one", description: "the first one"},
+      {content: text + " number two", description: "the second entry"}
+    ]);
+  });
+
+// This event is fired with the user accepts the input in the omnibox.
+chrome.omnibox.onInputEntered.addListener(
+  function(text) {
+    console.log('inputEntered: ' + text);
+    alert('You just typed "' + text + '"');
+  });
+*/
+
+  chrome.omnibox.onInputEntered.addListener(tolink => getLinkfromDB(`http://o/${tolink}`));
--- content.js
+++ content.js
@@ -1,21 +1,69 @@
-// gets the active tab's link and assiging it to the link input value of adding new link panel
-chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-  document.getElementById('link').value = tabs[0].url;
+let state, originalLink, projectName;
+
+// Defining a port to exchange messages between content "content.js" and background script "background.js"
+const port = chrome.runtime.connect({name: 'newLink'});
+
+// check if project name already exists in database
+document.getElementById('project').addEventListener('input', function projectNameVerifyInit() {
+  projectName = document.getElementById('project').value;
+  if (projectName !== '') {
+  let type = 'verify';
+  sendMessage(projectName, originalLink, type);
+}
 });
 
 // retrieving and storing new link panel values to variables
-document.getElementById('addLink').addEventListener('click', function getNewLinkAttr() {
-  var projectName = document.getElementById('project').value;
-  var originalLink = document.getElementById('link').value;
-  var createdAt = Math.round(new Date().getTime()/1000);
-  sendMessage(projectName, originalLink, createdAt);
+document.getElementById('addLink').addEventListener('click', function getNewLinkAttrInit() {
+  let type = 'add';
+  sendMessage(projectName, originalLink, type);
 });
 
-// Defining a port to exchange messages between popup and background scripts
-const port = chrome.runtime.connect({name: "newLink"});
+// sends new link values as an object to background script "background.js"
+const sendMessage = (projectName, originalLink, type) =>
+  port.postMessage({projectName: projectName, originalLink: originalLink, type: type});
 
-// sending new link values as an object to background script "contoller.js"
-function sendMessage(projectName, originalLink, createdAt){
-    port.postMessage({projectName: projectName, originalLink: originalLink, createdAt: createdAt});
-}
 
+// Disable add new link button at the popup page "popup.html"
+const disableAddButton = () => document.getElementById('addLink').disabled = true;
+
+// Enable add new link button at the popup page "popup.html"
+const enableAddButton = () => document.getElementById('addLink').disabled = false;
+
+// Recieve messages from the background script "background.js"
+port.onMessage.addListener(m => {
+
+  // active tab url
+  originalLink = m.taburl
+
+  // project name verification response
+  state = m.state;
+
+  // gets the active tab url and assiging it to the link input value of adding new link panel
+  document.getElementById('link').value = originalLink;
+  
+  switch(state) {  
+    case 'projectNameExist':
+      document.getElementById('projectNameExists').classList.remove('display');
+      disableAddButton();
+    break;
+    case 'projectNameDoesnotExist':
+      document.getElementById('projectNameExists').classList.add('display');
+      enableAddButton();
+    break;
+    case 'dbError':
+      document.getElementById('dbError').classList.remove('display');
+      disableAddButton();
+    break;
+    case 'linkAddedSuccessfully':
+      document.getElementById('succsed').classList.remove('display');
+      document.getElementById('succsed').insertAdjacentHTML('afterend', `<a href='#' class='success'>http://o/${projectName}</a>`);
+      document.getElementById('project').value = '';
+    break;
+    default:
+      disableAddButton();
+  }
+})
+
+var form = document.getElementById("newLinkForm");
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
