ca12e9778d018c181f4e71a89397c10eef3c2655
Hamoly <Hamoly@hotmail.com>
2020-06-28 02:01:27 +0200

    Code Styling

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ var db, tabid, msgPort;
 // Create indexedDB Request if not found
 let dbOpenRequest = indexedDB.open('onlinks', 2);
 // Create objectstore if not found
-dbOpenRequest.onupgradeneeded = e => { 
+dbOpenRequest.onupgradeneeded = e => {
   // saving opened DB to "db" variable
   db = e.target.result;
   // Creating objectStore on this database
@@ -23,19 +23,19 @@ chrome.runtime.onConnect.addListener(port => {
   // saving port result to use it later
   msgPort = port;
   // getting current tab URL
-  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+  chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs) {
     activeTabURL = tabs[0].url
-    msgPort.postMessage({taburl : activeTabURL});
+    msgPort.postMessage({ taburl: activeTabURL });
   });
 
   // Recieving message from popup if port name is the same
-  port.onMessage.addListener( msg => {
+  port.onMessage.addListener(msg => {
     // Checking the recived message port if the same as assigned at popup
-    if(port.name == 'newLink' && msg.type == 'add') {   
-    // Firing addNewLinkObj function with the recieved message to add the link to indexeddb
+    if (port.name == 'newLink' && msg.type == 'add') {
+      // Firing addNewLinkObj function with the recieved message to add the link to indexeddb
       addNewLinkObj(msg);
     }
-    if(port.name == 'newLink' && msg.type == 'verify') {   
+    if (port.name == 'newLink' && msg.type == 'verify') {
       // Firing addNewLinkObj function with the recieved message to add the link to indexeddb
       projectNameVerify(msg)
     }
@@ -44,21 +44,21 @@ chrome.runtime.onConnect.addListener(port => {
 
 // Add new link object
 const addNewLinkObj = msg => {
-  // Forming new link object and saving it to "newLink" variable 
-  let newLink = ({ key: `http://o/${msg.projectName}`, link: msg.originalLink });
+  // Forming new link object and saving it to "newLink" variable
+  let newLink = ({ key: `http://o/${ msg.projectName }`, link: msg.originalLink });
   // Openning a read/write db transaction, ready for adding the data
   let transactionRW = db.transaction(['shortenlink'], 'readwrite').objectStore('shortenlink');
   // Make a request to add new link object to the object store
   let addNewLinkReq = transactionRW.add(newLink);
   // Link Added Successfully
-  addNewLinkReq.onsuccess = () => msgPort.postMessage({taburl : activeTabURL, state: 'linkAddedSuccessfully'});
+  addNewLinkReq.onsuccess = () => msgPort.postMessage({ taburl: activeTabURL, state: 'linkAddedSuccessfully' });
   // Error adding link
-  addNewLinkReq.onerror = () => msgPort.postMessage({taburl : activeTabURL, state: 'dbError'});
+  addNewLinkReq.onerror = () => msgPort.postMessage({ taburl: activeTabURL, state: 'dbError' });
 }
 
 // check if entered project name already exists in DB
 const projectNameVerify = msg => {
-  let projectName = (`http://o/${msg.projectName}`);
+  let projectName = (`http://o/${ msg.projectName }`);
   console.log(projectName);
   // Openning a read only db transaction, ready for fetching data
   let transactionRO = db.transaction(['shortenlink'], 'readonly').objectStore('shortenlink');
@@ -67,12 +67,12 @@ const projectNameVerify = msg => {
   // Fetching original request link successed
   linkValueRequest.onsuccess = () => {
     if (linkValueRequest.result && linkValueRequest.result.link) {
-      msgPort.postMessage({taburl: activeTabURL, state: 'projectNameExist'});
+      msgPort.postMessage({ taburl: activeTabURL, state: 'projectNameExist' });
     } else {
-      msgPort.postMessage({taburl: activeTabURL, state: 'projectNameDoesnotExist'});
+      msgPort.postMessage({ taburl: activeTabURL, state: 'projectNameDoesnotExist' });
     }
   }
-  linkValueRequest.onerror = () => msgPort.postMessage({taburl: activeTabURL, state: 'dbError'});
+  linkValueRequest.onerror = () => msgPort.postMessage({ taburl: activeTabURL, state: 'dbError' });
 };
 ////////
 // REDIRECTING REQUESTED LINK
@@ -83,11 +83,11 @@ chrome.webNavigation.onBeforeNavigate.addListener(tab => {
   // saving tab.url
   tolink = tab.url
   // fire getLinkfromDB if tab url contains onlink/*
-  if(/o\/(.)/gi.test(tolink)) {
+  if (/o\/(.)/gi.test(tolink)) {
     getLinkfromDB(tolink);
   } else {
-      console.log('nay');
-    }
+    console.log('nay');
+  }
 });
 // fetching link and redirect if found
 const getLinkfromDB = tolink => {
@@ -100,7 +100,7 @@ const getLinkfromDB = tolink => {
     // saving the link value from the result of transaction
     let linkValue = linkValueRequest.result.link
     // updating tab url by the link value fo DB
-    chrome.tabs.update(tabid, {url: linkValue});
+    chrome.tabs.update(tabid, { url: linkValue });
   };
   linkValueRequest.onerror = err => console.log(err);
 };
@@ -123,4 +123,4 @@ chrome.omnibox.onInputEntered.addListener(
   });
 */
 
-  chrome.omnibox.onInputEntered.addListener(tolink => getLinkfromDB(`http://o/${tolink}`));
+chrome.omnibox.onInputEntered.addListener(tolink => getLinkfromDB(`http://o/${ tolink }`));
